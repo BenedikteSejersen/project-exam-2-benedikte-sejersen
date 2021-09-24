a950a8fc88d4558aa84d1a2b36bd7557ff929078
Benedikte Sejersen <aina_benedikte@hotmail.com>
2021-09-24 22:05:40 +0200

    nn

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack')
 
 module.exports = {
   reactStrictMode: true,
-  externalsPresets: { web: false, webAsync: true },
+  // externalsPresets: { web: false, webAsync: true },
   eslint: {
     ignoreDuringBuilds: true,
   },
